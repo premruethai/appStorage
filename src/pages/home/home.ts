c7cda8207d41cc9fc67d879ba4db5bdac8f62940
premruethai <pramrutai34@gmail.com>
2019-04-06 16:33:12 +0700

    Set ข้อมูล ลง local storage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Storage} from '@ionic/storage';
+import {GetvaluePage} from '../getvalue/getvalue';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public storage:Storage) {
+    storage.ready().then(
+      ()=>{
+        //Set ข้อมูล ลง local storage
+        storage.set("username",'Prem');
+        storage.set("studentId",'4568762136');
+      }
+    );
   }
 
 }
